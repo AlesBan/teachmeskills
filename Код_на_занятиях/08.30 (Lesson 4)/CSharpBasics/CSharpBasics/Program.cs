ad98bd695dcb71a0a26076df112bc7d8f0ca0579
Ales Ban <alesbah04@gmail.com>
2022-09-01 19:17:03 +0300

    Add DZ 01.09.2022

---
+++ Код_на_занятиях/08.30 (Lesson 4)/CSharpBasics/CSharpBasics/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using static System.Console;
+
+namespace CSharpBasics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a  = 8
+            
+        }
+
+    }
+}
---
+++ Код_на_занятиях/08.30 (Lesson 4)/Calculator/Calculator/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using static System.Console;
+
+namespace Calculator
+{
+    static class Program
+    {
+        public readonly static string[] opers = new string[] { "+", "-", "*", "/", "%", "^", "Z" };
+        public static void Main(string[] args)
+        {
+            Write("Приветствую тебя в нашем калькуляторе!");
+            WriteLine("Доступные операции: +\n-\n*\n/\n%\n^\nВыйти - Z");
+            CalculatorFoo();
+        }
+        public static void CalculatorFoo()
+        {
+            string oper = string.Empty;
+            int firstNum, secondNum;
+            while (oper != "Z")
+            {
+                WriteLine("Введите операцию");
+                oper = ReadLine();
+                if (Array.IndexOf(opers, oper) == -1)
+                {
+                    WriteLine("Некорректный ввод");
+                    continue;
+                }
+                try
+                {
+                    WriteLine("Введите первое число");
+                    firstNum = int.Parse(ReadLine());
+                    WriteLine("Введите второе число");
+                    secondNum = int.Parse(ReadLine());
+                }
+                catch
+                {
+                    WriteLine("!!!Вводите числа!!!");
+                    continue;
+                }
+
+                switch (oper)
+                {
+                    case "+" :
+                        {
+                            WriteLine($"{firstNum} + {secondNum} = {firstNum + secondNum}");
+                            break;
+                        }
+                    case "-":
+                        {
+                            WriteLine($"{firstNum} - {secondNum} = {firstNum - secondNum}");
+                            break;
+                        }
+                    case "*":
+                        {
+                            WriteLine($"{firstNum} * {secondNum} = {firstNum * secondNum}");
+                            break;
+                        }
+                    case "/":
+                        {
+                            WriteLine($"{firstNum} / {secondNum} = {firstNum / secondNum}");
+                            break;
+                        }
+                    case "%":
+                        {
+                            WriteLine($"{firstNum} / {secondNum} = {firstNum / 100 * secondNum}");
+                            break;
+                        }
+                    case "^":
+                        {
+                            WriteLine($"{firstNum} ^ {secondNum} = {Math.Pow(firstNum, secondNum)}");
+                            break;
+                        }
+                }
+            }
+            WriteLine("Конец.");
+        }
+    }
+}
