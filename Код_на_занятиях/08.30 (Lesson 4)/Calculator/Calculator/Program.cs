74301c81b5c79432c9531f611f4766255d3d6c11
Ales Ban <alesbah04@gmail.com>
2022-09-01 20:10:29 +0300

    Add calculator

--- Код_на_занятиях/08.30 (Lesson 4)/Calculator/Calculator/Program.cs
+++ Код_на_занятиях/08.30 (Lesson 4)/Calculator/Calculator/Program.cs
@@ -5,17 +5,18 @@ namespace Calculator
 {
     static class Program
     {
-        public readonly static string[] opers = new string[] { "+", "-", "*", "/", "%", "^", "Z" };
+        public readonly static string[] opers = new string[] { "+", "-", "*", "/", "%", "^", "SR", "Z" };
         public static void Main(string[] args)
         {
-            Write("Приветствую тебя в нашем калькуляторе!");
-            WriteLine("Доступные операции: +\n-\n*\n/\n%\n^\nВыйти - Z");
             CalculatorFoo();
         }
+
         public static void CalculatorFoo()
         {
+            Write("Приветствую тебя в нашем калькуляторе!");
+            WriteLine("Доступные операции: +\n-\n*\n/\n%\n^\nSquere root - SR\nВыйти - Z");
             string oper = string.Empty;
-            int firstNum, secondNum;
+            double firstNum, secondNum;
             while (oper != "Z")
             {
                 WriteLine("Введите операцию");
@@ -24,6 +25,21 @@ namespace Calculator
                 {
                     WriteLine("Некорректный ввод");
                     continue;
+                }
+                if (oper == "SR")
+                {
+                    try
+                    {
+                        WriteLine("Введите число");
+                        firstNum = int.Parse(ReadLine());
+                        WriteLine(SquareRootOper(firstNum));
+                        continue;
+                    }
+                    catch
+                    {
+                        WriteLine("!!!Вводите числа!!!");
+                        continue;
+                    }
                 }
                 try
                 {
@@ -37,42 +53,87 @@ namespace Calculator
                     WriteLine("!!!Вводите числа!!!");
                     continue;
                 }
-
-                switch (oper)
-                {
-                    case "+" :
-                        {
-                            WriteLine($"{firstNum} + {secondNum} = {firstNum + secondNum}");
-                            break;
-                        }
-                    case "-":
-                        {
-                            WriteLine($"{firstNum} - {secondNum} = {firstNum - secondNum}");
-                            break;
-                        }
-                    case "*":
-                        {
-                            WriteLine($"{firstNum} * {secondNum} = {firstNum * secondNum}");
-                            break;
-                        }
-                    case "/":
-                        {
-                            WriteLine($"{firstNum} / {secondNum} = {firstNum / secondNum}");
-                            break;
-                        }
-                    case "%":
-                        {
-                            WriteLine($"{firstNum} / {secondNum} = {firstNum / 100 * secondNum}");
-                            break;
-                        }
-                    case "^":
-                        {
-                            WriteLine($"{firstNum} ^ {secondNum} = {Math.Pow(firstNum, secondNum)}");
-                            break;
-                        }
-                }
+                Operations(oper, firstNum, secondNum);
             }
             WriteLine("Конец.");
+        }
+        public static void Operations(string oper, double firstNum, double secondNum)
+        {
+            double result = 0;
+            switch (oper)
+            {
+                case "+":
+                    {
+                        result = PlusOper(firstNum, secondNum);
+                    break;
+                    }
+                case "-":
+                    {
+                        result = MinusOper(firstNum, secondNum);
+                        break;
+                    }
+                case "*":
+                    {
+                        result = MultiplyOper(firstNum, secondNum);
+                        break;
+                    }
+                case "/":
+                    {
+                        result = DivideOper(firstNum, secondNum);
+                        break;
+                    }
+                case "%":
+                    {
+                        result = PersentOper(firstNum, secondNum);
+                        break;
+                    }
+                case "^":
+                    {
+                        result = DegreeOper(firstNum, secondNum);
+                        break;
+                    }
+                default:
+                    {
+                        break;
+                    }
+            }
+            WriteLine($"{firstNum} {oper} {secondNum} = {result}");
+        }
+        //  +
+        public static double PlusOper(double firstNum, double secondNum)
+        {
+            return firstNum + secondNum;
+        }
+        //  -
+        public static double MinusOper(double firstNum, double secondNum)
+        {
+            return firstNum - secondNum;
+        }
+        //  *
+        public static double MultiplyOper(double firstNum, double secondNum)
+        {
+            return firstNum * secondNum;
+        }
+        //  /
+        public static double DivideOper(double firstNum, double secondNum)
+        {
+            return firstNum / secondNum;
+        }
+        //  %
+        public static double PersentOper(double firstNum, double secondNum)
+        {
+            return firstNum / 100 * secondNum;
+        }
+        // ^
+        public static double DegreeOper(double firstNum, double secondNum)
+        {
+            return Math.Pow(firstNum, secondNum);
+        }
+
+        // SR
+        public static string SquareRootOper(double firstNum)
+        {
+            return $"Sqare root of {firstNum} = {Math.Sqrt(firstNum)}";
         }
     }
 }
