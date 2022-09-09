0fbeec6819b7912adbf67f971b393c394ae43138
Ales Ban <alesbah04@gmail.com>
2022-09-09 15:46:01 +0300

    add in Calculator until 4 task

--- HW/08.30 - Lesson 4/Calculator/Calculator/Program.cs
+++ HW/08.30 - Lesson 4/Calculator/Calculator/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using static System.Console;
 
@@ -6,10 +8,11 @@ namespace Calculator
 {
     static class Program
     {
-        public readonly static string[] allOperations = new string[] { "+", "-", "*", "/", "%", "^", "КК", $"{ExitOper}" };
+        public readonly static string[] allOperations = new string[] { "+", "-", "*", "/", "%", "^", "Sqrt", $"{ExitOper}" };
+        public readonly static string[] allMathOperations = new string[] { "+", "-", "*", "/", "%", "^", "Sqrt"};
         public readonly static string[] opersWithTwoNums = new string[] { "+", "-", "*", "/", "%", "^"};
-        public readonly static string[] opersWithOneNum = new string[] { "КК" };
-        public readonly static string availableOperations = $"Доступные операции: \n{string.Join(" ", allOperations)}\nКК - Square root";
+        public readonly static string[] opersWithOneNum = new string[] { "Sqrt" };
+        public readonly static string availableOperations = $"Доступные операции: \n{string.Join(" ", allOperations)}\nSqrt - Квадратный корень\n{ExitOper} - Выход";
         public const string ExitOper = "Z";
         public readonly static string[] wordsYes = new string[] { "ДА","Да","да","lf","LF","Lf" };
         public readonly static string[] wordsNo = new string[] { "YTN", "Ytn", "ytn", "нет", "Нет", "НЕТ" };
@@ -20,12 +23,12 @@ namespace Calculator
         public static void CalculatorGreed()
         {
             WriteLine("Приветствую тебя в нашем калькуляторе!");
+            WriteLine($"{availableOperations}\nТакже можете вводить пример сразу\n{ExitOper} - Выход");
             CalculatorMain();
             WriteLine("Пока.");
         }
         public static void CalculatorMain()
         {
-            WriteLine($"{availableOperations}\nТакже можете вводить пример сразу\n{ExitOper} - Выход");
             string oper = GetOperation();
 
             while (oper != ExitOper)
@@ -34,7 +37,7 @@ namespace Calculator
                 {
                     if (Array.IndexOf(allOperations, oper) == -1)
                     {
-                        WriteLine("Некорректный ввод");
+                        IncorrectInput_Message();
                         GetOperation();
                     }
                     Calculate_ExampleInParts(oper);
@@ -52,58 +55,78 @@ namespace Calculator
             string oper = ReadLine();
             return oper;
         }
-        public static void Calculate_WholeExample(string oper)
+        public static void Calculate_WholeExample(string example)
         {
-            string[] ExampleParts = oper.Split(" ");
-            try
+            bool oneNum = false;
+            string operatorEx = string.Empty;
+
+            foreach (string op in opersWithOneNum.Where(x => example.Contains(x)))
             {
-                WriteLine(CaclResult(ExampleParts[1], int.Parse(ExampleParts[0]), int.Parse(ExampleParts[2])));
+                oneNum = true;
+                operatorEx = op;
             }
-            catch
+            if (oneNum)
             {
-                WriteLine("Некорректный ввод");
-                CalculatorMain();
+                WriteLine(CaclResult(operatorEx, double.Parse(example.Replace(operatorEx,"").Trim()), default));
+            }
+            else
+            {
+                foreach (string oper in allMathOperations.Where(x => example.Contains(x)))
+                {
+                    operatorEx = oper;
+                }
+                List<string> ExampleNums = new List<string>
+                {
+                    example[..example.IndexOf(operatorEx)].Trim(),
+                    example[(example.IndexOf(operatorEx)+1)..].Trim()
+                };
+                foreach (string str in ExampleNums)
+                {
+                    WriteLine(str + "\n");
+                }
+                try
+                {
+                    WriteLine(CaclResult(operatorEx, double.Parse(ExampleNums[0]), double.Parse(ExampleNums[1])));
+                }
+                catch
+                {
+                    IncorrectInput_Message();
+                    CalculatorMain();
+                }
             }
         }
         public static void Calculate_ExampleInParts(string oper)
         {
-            try
+            if (Array.IndexOf(opersWithTwoNums, oper) != -1)
             {
-                if (Array.IndexOf(opersWithTwoNums, oper) != -1)
-                {
-                    InputTwoNums_Calculate(oper);
-                }
-                else if (Array.IndexOf(opersWithOneNum, oper) != -1)
-                {
-                    InputOneNum_Calculate(oper);
-                }
-                else
-                {
-                    WriteLine("!!!Вводите операции корректно!!!");
-                }
+                InputTwoNums_Calculate(oper);
             }
-            catch
+            else if (Array.IndexOf(opersWithOneNum, oper) != -1)
             {
-                WriteLine("!!!Вводите числа!!!");
+                InputOneNum_Calculate(oper);
             }
         }
         public static void InputTwoNums_Calculate(string oper)
         {
             WriteLine("Введите первое число");
-            double firstNum = int.Parse(ReadLine());
+            double firstNum = double.Parse(ReadLine());
             WriteLine("Введите второе число");
-            double secondNum = int.Parse(ReadLine());
+            double secondNum = double.Parse(ReadLine());
             WriteLine(CaclResult(oper, firstNum, secondNum));
         }
         public static void InputOneNum_Calculate(string oper)
         {
             WriteLine("Введите число");
-            double firstNum = int.Parse(ReadLine());
+            double firstNum = double.Parse(ReadLine());
             WriteLine(CaclResult(oper, firstNum, (int)default));
         }
         public static string CaclResult(string oper, double firstNum, double secondNum)
         {
             double result;
+            int decimalPointFirstNum = 1, decimalPointSecondNum = 1;
+            while (firstNum * Math.Pow(10, 1 + decimalPointFirstNum) % 10 != 0) { decimalPointFirstNum++; }
+            while (secondNum * Math.Pow(10, 1 + decimalPointSecondNum) % 10 != 0) { decimalPointSecondNum++; }
+            int totalDecimalPoint = decimalPointFirstNum > decimalPointSecondNum ? decimalPointFirstNum : decimalPointSecondNum;
             switch (oper)
             {
                 case "+": result = PlusOper(firstNum, secondNum); break;
@@ -118,19 +141,19 @@ namespace Calculator
 
                 case "^": result = DegreeOper(firstNum, secondNum); break;
 
-                case "SR": return SquareRootOper(firstNum);
+                case "Sqrt": return SquareRootOper(firstNum);
 
                 default:
                     {
                         return "Такую операцию наш калькулятор не поддерживает!";
-                        break;
                     }
             }
-            return $"{firstNum} {oper} {secondNum} = {result}";
+            return $"{firstNum} {oper} {secondNum} = {Math.Round(result, totalDecimalPoint)}";
         }
         public static string AskForContinue()
         {
-            WriteLine("Желаете продолжить? Да/Нет");
+            WriteLine("\nЖелаете продолжить? Да/Нет");
+            WriteLine($"Также можете вводить пример сразу\n{availableOperations}\n");
             string choise = ReadLine();
             if (Array.IndexOf(wordsYes, choise) != -1)
             {
@@ -140,9 +163,25 @@ namespace Calculator
             {
                 return ExitOper;
             }
+            else if (choise == ExitOper)
+            {
+                return ExitOper;
+            }
+            if (choise.Length > 2)
+            {
+                foreach (string oper in allMathOperations)
+                {
+                    if (choise.Where(x => x.ToString() == oper) != null)
+                    {
+                        return choise;
+                    }
+                }
+                IncorrectInput_Message();
+                return AskForContinue();
+            }
             else
             {
-                WriteLine("Некоррекный ввод");
+                IncorrectInput_Message();
                 return AskForContinue();
             }
         }
@@ -173,6 +212,10 @@ namespace Calculator
         public static string SquareRootOper(double firstNum)
         {
             return $"Square root of {firstNum} = {Math.Sqrt(firstNum)}";
+        }
+        public static void IncorrectInput_Message()
+        {
+            WriteLine("Некоррекный ввод");
         }
     }
 }
