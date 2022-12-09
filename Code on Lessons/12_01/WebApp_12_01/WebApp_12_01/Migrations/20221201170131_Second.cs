39d285a8527b07be006a34d52fb9e6f9e82921c9
Ales Ban <alesbah04@gmail.com>
2022-12-09 12:20:50 +0300

    Lesson 12_01

---
+++ Code on Lessons/12_01/WebApp_12_01/WebApp_12_01/Migrations/20221201170131_Second.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebApp_12_01.Migrations
+{
+    public partial class Second : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
