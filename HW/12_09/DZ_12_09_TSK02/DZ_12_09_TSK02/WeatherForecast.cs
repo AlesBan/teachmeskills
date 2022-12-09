956d5291257a35e9f8b58cfc4bf812347e4192da
Ales Ban <alesbah04@gmail.com>
2022-12-09 12:27:30 +0300

    crete project

---
+++ HW/12_09/DZ_12_09_TSK02/DZ_12_09_TSK02/WeatherForecast.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace DZ_12_09_TSK02
+{
+    public class WeatherForecast
+    {
+        public DateTime Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string Summary { get; set; }
+    }
+}
