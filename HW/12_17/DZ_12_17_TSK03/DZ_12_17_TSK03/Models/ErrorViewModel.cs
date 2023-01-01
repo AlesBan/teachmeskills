0fb3cdfd1fd299621e101f662263a6bd0361c5ad
Ales Ban <alesbah04@gmail.com>
2023-01-01 22:25:19 +0300

    aboba

--- HW/12_17/DZ_12_17_TSK03/DZ_12_17_TSK03/Models/ErrorViewModel.cs
+++ HW/12_17/DZ_12_17_TSK03/DZ_12_17_TSK03/Models/ErrorViewModel.cs
@@ -2,7 +2,7 @@ namespace DZ_12_17_TSK03.Models;
 
 public class ErrorViewModel
 {
-    public string? RequestId { get; set; }
+    public string? RequestId { get; set; } 
 
     public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
 }
