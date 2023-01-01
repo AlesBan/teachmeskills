2ad907ab985016a3a60045d75a5f30b02c1db8eb
Ales Ban <alesbah04@gmail.com>
2023-01-01 22:21:54 +0300

    halg of 30 Lesson

--- Code on Lessons/12_23/WebApplication3/JWTAuth/Controllers/WeatherForecastController.cs
+++ Code on Lessons/12_23/WebApplication3/JWTAuth/Controllers/WeatherForecastController.cs
@@ -1,4 +1,14 @@
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using JWTAuth.Models;
+using JWTAuth.Policies;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
 
 namespace JWTAuth.Controllers;
 
@@ -6,19 +16,31 @@ namespace JWTAuth.Controllers;
 [Route("[controller]")]
 public class WeatherForecastController : ControllerBase
 {
+    public static List<WeatherUser> Users = new List<WeatherUser>()
+    {
+        new WeatherUser()
+        {
+            UserName = "ales",
+            Password = "admin",
+            Role = new List<string>(new[] { "admin", "user" })
+        }
+    };
     private static readonly string[] Summaries = new[]
     {
         "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
     };
 
     private readonly ILogger<WeatherForecastController> _logger;
+    private readonly string _securitykey;
 
-    public WeatherForecastController(ILogger<WeatherForecastController> logger)
+    public WeatherForecastController(ILogger<WeatherForecastController> logger, IConfiguration configuration)
     {
         _logger = logger;
+        _securitykey = configuration["JWTSecurityKey"];
     }
 
     [HttpGet(Name = "GetWeatherForecast")]
+    [Authorize(Policy = NamePolicy.Policy)]
     public IEnumerable<WeatherForecast> Get()
     {
         return Enumerable.Range(1, 5).Select(index => new WeatherForecast
@@ -28,5 +50,28 @@ public class WeatherForecastController : ControllerBase
                 Summary = Summaries[Random.Shared.Next(Summaries.Length)]
             })
             .ToArray();
+    }
+
+    [HttpGet("login")]
+    public string Login([FromQuery] string userName)
+    {
+        var user = Users.Single(x => x.UserName == userName);
+        var claims = user.Role.Select(x => new Claim(ClaimTypes.Role, x)).ToList(); //Выдаем пользователю роль под видом клейма
+        claims.Add(new Claim(ClaimTypes.Name, userName));
+
+        var claimIdentity = new ClaimsIdentity(claims, JwtBearerDefaults.AuthenticationScheme);
+        var userClaimsPrincipal = new ClaimsPrincipal(claimIdentity);
+
+        var tokenDescr = new SecurityTokenDescriptor()
+        {
+            Expires = DateTime.Now.AddDays(7),
+            Subject = claimIdentity,
+            SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(Encoding.ASCII.GetBytes(_securitykey)), 
+                SecurityAlgorithms.HmacSha256Signature)
+        };
+        
+        var tokenHandler = new JwtSecurityTokenHandler();
+        var token =  tokenHandler.CreateToken(tokenDescr);
+        return tokenHandler.WriteToken(token);
     }
 }
--- Code on Lessons/12_23/WebApplication3/WebApplication1/WeatherForecast.cs
+++ Code on Lessons/12_23/WebApplication3/WebApplication1/WeatherForecast.cs
@@ -1,4 +1,6 @@
-namespace JWTAuth
+using System;
+
+namespace WebApplication1
 {
     public class WeatherForecast
     {
@@ -8,6 +10,6 @@ namespace JWTAuth
 
         public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 
-        public string? Summary { get; set; }
+        public string Summary { get; set; }
     }
 }
